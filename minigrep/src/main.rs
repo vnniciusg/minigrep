08b768191160e02d1f71efda049e19e32204d5ab
vnn <vinnicius109@gmail.com>
2026-02-07 13:00:07 -0300

    feat: working with environment variables

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs;
 use std::process;
 use std::error::Error;
-use minigrep::search;
+use minigrep::{search, search_case_insensitive};
 
 
 fn main() {
@@ -24,8 +24,14 @@ fn main() {
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> { 
     let contents = fs::read_to_string(config.file_path)?;
+    
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
 
-    for line in search(&config.query, &contents) {
+    for line in results  {
         println!("{line}");
     }   
 
@@ -33,8 +39,9 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 struct Config {
-    query: String,
-    file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -45,7 +52,10 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
+        
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path })
+
+        Ok(Config { query, file_path, ignore_case, })
     }
 }
